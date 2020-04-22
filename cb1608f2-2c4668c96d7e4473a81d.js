4b78b3af0004fb94afc2833060e8232c5362209c
Abhijeet Dhamankar <adhamankar@gmail.com>
2020-04-22 19:32:58 +0530

    Updates

---
+++ cb1608f2-2c4668c96d7e4473a81d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{wHSu:function(c,n,i){"use strict";i.d(n,"a",(function(){return o}));var o={prefix:"fas",iconName:"chevron-right",icon:[320,512,[],"f054","M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"]}}}]);
+//# sourceMappingURL=cb1608f2-2c4668c96d7e4473a81d.js.map
